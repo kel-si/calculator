7c5993d5c2049a04ee340892d3c11a33306c436b
Kelsi Olstad <kelsiolstad@gmail.com>
2022-05-18 15:09:01 -0600

    import useState, setState for number depending on what button is clicked

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,12 +1,26 @@
+import { useState } from "react";
+
 import Screen from "./Screen";
-import ButtonList from "./ButtonList";
+import Buttons from "./Buttons";
+
 import "../styles/index.css";
 
 export default function Container() {
+  let [input, setInput] = useState({
+    operator: "",
+    number: 0,
+    result: "0",
+  });
+
+  function handleClick(e) {
+    setInput({ operator: "", number: 0, result: e.target.name });
+    console.log("e", e.target.name);
+  }
+
   return (
     <div className="calc-container">
-      <Screen />
-      <ButtonList />
+      <Screen result={input.number ? input.number : input.result} />
+      <Buttons handleClick={handleClick} number={input.number} />
     </div>
   );
 }
