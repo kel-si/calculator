1f969086b90468e75b9351d0f21fab6bf63c595e
Kelsi Olstad <kelsiolstad@gmail.com>
2022-05-18 23:28:49 -0600

    add header

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Container from "./components/Container";
 export default function App() {
   return (
     <div className="calculator">
+      <h1>React Calculator App</h1>
       <Container />
     </div>
   );
