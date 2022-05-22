88b8b4322d0aa68516eec822d7518e6ed2d3958f
Kelsi Olstad <kelsiolstad@gmail.com>
2022-05-22 11:02:46 -0600

    fix bug - check length before the decimal

--- src/components/Screen.jsx
+++ src/components/Screen.jsx
@@ -1,9 +1,15 @@
 export default function Screen(props) {
   //convert to string to set a maximum length for the result
-  const resultsStr = props.result.toString().substring(0, 9);
+  const resultsStr = props.result.toString();
+
+  //check length of digits in front of decimal place (too big of a number?) cut off extra decimal places if needed
+  const wholeNum = resultsStr.split(".");
+
   return (
     <div className="calc-screen">
-      <div className="result">{resultsStr}</div>
+      <div className="result">
+        {wholeNum.length > 9 ? "ERROR" : resultsStr.substring(0, 9)}
+      </div>
     </div>
   );
 }
